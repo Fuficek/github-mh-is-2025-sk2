0a6ff7b560bac04a0a18c13645b14a48215a561d
Matěj Hrabal <matalacek49fuf@gmail.com>
2025-10-16 13:34:51 +0200

    Dokončena funkcionalita program000a-zakladni-kod
    - Přidán vstup pro první číslo, poslední číslo a rozdíl
    - Implementována validace pro všechny tři vstupy
    - Přidána logická kontrola platnosti kombinace hodnot
    - Implementováno generování číselné řady pro kladný i záporný step
    - Program nyní plně funkční podle zadání

--- IS-Programy/program000a-zakladni-kod/Program.cs
+++ IS-Programy/program000a-zakladni-kod/Program.cs
@@ -24,12 +24,49 @@ while (again == "a")
         Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
     }
 
+    Console.Write("Zadejte poslední číslo řady (celé číslo): ");
+    int last;
+    while (!int.TryParse(Console.ReadLine(), out last))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte poslední číslo řady znovu: ");
+    }
 
+    Console.Write("Zadejte rozdíl mezi čísly (celé číslo): ");
+    int step;
+    while (!int.TryParse(Console.ReadLine(), out step))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte rozdíl znovu: ");
+    }
 
-
-
-
-
+    if (step == 0)
+    {
+        Console.WriteLine("Rozdíl nemůže být 0!");
+    }
+    else if ((step > 0 && first > last) || (step < 0 && first < last))
+    {
+        Console.WriteLine("Neplatná kombinace hodnot - řada nebude konečná!");
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine("Číselná řada:");
+        
+        if (step > 0)
+        {
+            for (int i = first; i <= last; i += step)
+            {
+                Console.Write(i + " ");
+            }
+        }
+        else
+        {
+            for (int i = first; i >= last; i += step)
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.WriteLine();
+    }
 
     Console.WriteLine();
     Console.WriteLine("Pro opakování programu stiskněte klávesu a");
